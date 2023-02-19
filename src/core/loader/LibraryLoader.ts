b2244eafad6d695886146750d056768c57b4260f
dotrey <dotrey@gmail.com>
2023-02-19 17:26:55 +0100

    added test for library loader

--- src/core/loader/LibraryLoader.ts
+++ src/core/loader/LibraryLoader.ts
@@ -1,18 +1,19 @@
 import { Library } from "../model/Library";
+import { PathMerger } from "../utility/PathMerger";
 import { FileLoader } from "./FileLoader";
 import { LibraryParser } from "./LibraryParser";
 
 export class LibraryLoader {
     private parser: LibraryParser;
 
-    constructor(private fileLoader: FileLoader) {
+    constructor(private fileLoader: FileLoader, private pathMerger: PathMerger) {
         this.parser = new LibraryParser();
     }
 
     async load(file: string): Promise<Library> {
         try {
             let content = await this.fileLoader.load(file);
-            return this.parser.parse(content, file, this.fileLoader);
+            return this.parser.parse(content, this.fileLoader, this.pathMerger);
         } catch (e) {
             return this.buildErrorLibrary((e as Error).message, file)
         }
--- test/index.md
+++ test/index.md
@@ -1,4 +1,12 @@
 # Test Cards
 These files contain cards used in tests, do not modify without checking the tests.
 
+## Book 1
 - [Cardset 1](./cards/cardset-1.md)
+
+## Book 2
+- [Cardset 1](./cards/cardset-1.md)
+- [Cardset 2](./cards/cardset-2.md)
+
+## Book 3
+- [Cardset 2](./cards/cardset-2.md)
